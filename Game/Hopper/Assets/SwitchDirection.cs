eb485e712fabbaaf9bec9ae1d9cdf4be5a5d702e
CJace33 <s176047@UCS.ac.uk>
2017-10-12 21:54:57 +0100

    Finished initial level and have moving switching buttons

--- Game/Hopper/Assets/SwitchDirection.cs
+++ Game/Hopper/Assets/SwitchDirection.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SwitchDirection : MonoBehaviour {
 
+    private SpriteRenderer ThisObjectRenderer;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+	    
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    
 	}
+    void Awake()
+    {
+        ThisObjectRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseDown()
+    {
+        //Flip this objects X when the object is clicked on
+        ThisObjectRenderer.flipX = !ThisObjectRenderer.flipX;
+    }
 }
