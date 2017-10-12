f537a84eba7a41737d540f1cc5ee1c2bfee14589
Connor Wilby <CJace33@users.noreply.github.com>
2017-10-12 14:37:06 +0100

    Uploaded prototype 2 (hopper) base
    Doesn't do a lot, mostly sorted prefabs and stand-in art

---
+++ Game/Hopper/Assets/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+    public GameObject RedToad, BlueToad, PinkToad;
+    public GameObject RedPad, BluePad, PinkPad;
+
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
