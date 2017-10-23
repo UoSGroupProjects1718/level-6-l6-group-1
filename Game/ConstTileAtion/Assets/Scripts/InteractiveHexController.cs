33ebaebadc0aa63d2f97d5a139bf9539281ca22a
CJace33 <s176047@UCS.ac.uk>
2017-10-23 20:23:08 +0100

    Added Layers system
    Allows easy addition of new layers and disabling of unused layers.

---
+++ Game/ConstTileAtion/Assets/Scripts/InteractiveHexController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveHexController : MonoBehaviour {
+
+    public Sprite sprite1, sprite2, sprite3;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
