3f8e8eecdf19ced863e374c226b1a2a78904f145
Connor Wilby <CJace33@gmail.com>
2017-10-31 12:35:15 +0000

    Worked on tile movement

--- Game/ConstTileAtion/Assets/Scripts/GameMaster.cs
+++ Game/ConstTileAtion/Assets/Scripts/GameMaster.cs
@@ -6,26 +6,32 @@ public class GameMaster : MonoBehaviour {
 
     public GameObject HexPrefab;
 
-    //Adds a header into the inspecter
-    [Header("Layers")]
+    //Storage for the Hexbackground GameObjects
+    public GameObject HexBackground_Center, HexBackground_1, HexBackground_2, HexBackground_3;
 
     //Gameobject which is the parent of all that rings hexes
     public GameObject RingHolder0, RingHolder1, RingHolder2, RingHolder3;
+    
     //Array to hold all hex gameobjects, serialised to show in the inspector
     [SerializeField] GameObject[]   HexRing0 = new GameObject[1],
                                     HexRing1 = new GameObject[6],
                                     HexRing2 = new GameObject[12],
                                     HexRing3 = new GameObject[18];
+    public GameObject TileHolder;
+
     [Header("GameInfo")]
-    public int LayersBeingUsed;
+    [Range(1,4)]public int LayersBeingUsed;
+    public int Moves;
+    public bool Clicked = false;
+    public GameObject CurrentlySelected;
 
 
     //Useful thing to use later on
     //HexInfo ChildScript = child.GetComponent<HexInfo>();
-    
-        
-        // Use this for initialization
-    void Start ()
+
+
+    // Use this for initialization
+    void Start()
     {
         //First, loop through each of the objects which are children of the ringholders and add them into the array
         GetHexChildren(RingHolder0, out HexRing0);
@@ -33,15 +39,28 @@ public class GameMaster : MonoBehaviour {
         GetHexChildren(RingHolder2, out HexRing2);
         GetHexChildren(RingHolder3, out HexRing3);
 
-        //Then go through each of the children and instantiate a hex in its place
+        //Then go through each of the children and instantiate a hex in its place.
+        //At the same time, set the correct layer of the background to active
         if (LayersBeingUsed >= 1)
+        {
             InstantiateLayers(HexRing0);
+            HexBackground_Center.SetActive(true);
+        }
         if (LayersBeingUsed >= 2)
+        {
             InstantiateLayers(HexRing1);
+            HexBackground_1.SetActive(true);
+        }
         if (LayersBeingUsed >= 3)
+        {
             InstantiateLayers(HexRing2);
+            HexBackground_2.SetActive(true);
+        }
         if (LayersBeingUsed >= 4)
+        {
             InstantiateLayers(HexRing3);
+            HexBackground_3.SetActive(true);
+        }
         
     }
 	
@@ -70,7 +89,7 @@ public class GameMaster : MonoBehaviour {
         {
             //Instantiate the Hex prefab in the location of the "hexs" guide
             GameObject NewHex = Instantiate(HexPrefab, new Vector3(Hexs[i].transform.position.x, Hexs[i].transform.position.y), Quaternion.identity);
-
+            NewHex.transform.parent = TileHolder.transform;
             //Get the script from the hex guide to get its coordinates
             HexInfo GuideScript = Hexs[i].GetComponent<HexInfo>();
 
--- Game/ConstTileAtion/Assets/Scripts/InteractiveHexController.cs
+++ Game/ConstTileAtion/Assets/Scripts/InteractiveHexController.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class InteractiveHexController : MonoBehaviour {
 
-    public Sprite Aries, Taurus, GeminiGood, GeminiBad, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces;
+    //Sprites for the various types of tile
+    [Header("Tile Sprites")]
+    public Sprite Aries;
+    public Sprite Taurus;
+    public Sprite GeminiGood;
+    public Sprite GeminiBad;
+    public Sprite Cancer;
+    public Sprite Leo;
+    public Sprite Virgo;
+    public Sprite Libra;
+    public Sprite Scorpio;
+    public Sprite Sagittarius;
+    public Sprite Capricorn;
+    public Sprite Aquarius;
+    public Sprite Pisces;
+   
 
+    //Enum to identify which tile this one is
     public enum HexType
     {
         Null,
@@ -24,13 +40,19 @@ public class InteractiveHexController : MonoBehaviour {
         Pisces
     }
 
+    //Enum holder
     public HexType CurrentHexType;
 
     //Hex's current arbitrary coordinates on the hex grid
     public int x, y;
 
-    //Sprite renderer, private because it will be set indevidually
+    //Sprite renderer, private because it will be set individually
     private SpriteRenderer SprRenderer;
+
+    //
+    GameObject GMaster;
+    GameMaster GMScript;
+
 
 
 	// Use this for initialization
@@ -38,8 +60,12 @@ public class InteractiveHexController : MonoBehaviour {
     {
         //Access the sprite renderer on startup
         SprRenderer = GetComponent<SpriteRenderer>();
+        
+        //Find the gamemaster object
+        GMaster = GameObject.Find("HexBaseHolder");
+        GMScript = GMaster.GetComponent<GameMaster>();
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -106,5 +132,75 @@ public class InteractiveHexController : MonoBehaviour {
                 break;
         }
     }
-    
+
+    public void OnMouseDown()
+    {
+        //If nothing has been selected yet
+        if (GMScript.Clicked == false)
+        {
+            //Set this as the currently selected hex
+            GMScript.CurrentlySelected = this.gameObject;
+            GMScript.Clicked = true;
+        }
+        //If something has been selected, check if its possible to switch the two
+        else
+        {
+            InteractiveHexController CurrentlySelectedScript = GMScript.CurrentlySelected.GetComponent<InteractiveHexController>();
+            //And if it is possible, switch them
+            //Record old position and X/Y
+            Vector3 OldPos = this.transform.position;
+            int OldX = x;
+            int OldY = y;
+
+            if (CompareX(CurrentlySelectedScript, 1) && CompareY(CurrentlySelectedScript, 1))
+            {
+                //Switch this with the previously selected tile and set its coordinates to be the new ones
+                this.transform.position = GMScript.CurrentlySelected.transform.position;
+                x = CurrentlySelectedScript.x;
+                y = CurrentlySelectedScript.y;
+
+                //Now switch the  
+                GMScript.CurrentlySelected.transform.position = OldPos;
+                CurrentlySelectedScript.y = OldY;
+                CurrentlySelectedScript.y = OldY;
+
+                GMScript.Clicked = false;
+                Debug.Log("Switched: " + OldX + "," + OldY + " With " + x + "," + y);
+            }
+            else
+            {
+                GMScript.Clicked = false;
+                Debug.Log("Can't switch: " + OldX + "," + OldY + " With " + x + "," + y);
+                return;
+            }
+            //Increment the moves counter
+            GMScript.Moves++;
+        }
+    }
+
+    //Compares this x to the x of the currently selected tile
+    public bool CompareX(InteractiveHexController Tile, int Tolerance)
+    {
+        //Compare this x to the 
+        if (this.x == Tile.x + Tolerance
+            ||
+            this.x == Tile.x - Tolerance)
+        {
+            return true;
+        }
+        
+        return false;
+    }
+    //Compares this y to the y of the currently selected tile
+    public bool CompareY(InteractiveHexController Tile, int Tolerance)
+    {
+        //Compare this x to the 
+        if (this.y == Tile.y + Tolerance
+            ||
+            this.y == Tile.y - Tolerance)
+        {
+            return true;
+        }
+        return false;
+    }
 }
